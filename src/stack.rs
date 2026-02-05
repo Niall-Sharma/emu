2bef0822383ed95550f84a492c50cf37b4a8ca01
Niall Sharma <niall.k.sharma@gmail.com>
2026-02-05 11:47:21 -0600

    First commit

---
+++ src/stack.rs
@@ -0,0 +1,48 @@
+use std::collections::HashMap;
+
+pub struct Stack {
+    size: u32,
+    stack_array: Vec<i32>,
+    stack_variables: HashMap<String, i32>,
+}
+
+impl Stack {
+    pub fn new() -> Self {
+        Self {
+            size: 0,
+            stack_array: Vec::new(),
+            stack_variables: HashMap::new(),
+        }
+    }
+
+    pub fn pop(&mut self) -> Result<i32, &'static str> {
+        match self.stack_array.pop() {
+            None => Err("Empty stack"),
+            Some(value) => {
+                self.size = self.size - 1;
+                Ok(value)
+            }
+        }
+    }
+
+    pub fn push(&mut self, op: i32) {
+        self.stack_array.push(op);
+        self.size = self.size + 1;
+    }
+
+    pub fn assign_variable(&mut self, var: String, value: &i32) -> Result<(), &'static str> {
+        self.stack_variables.insert(var, *value);
+        Ok(())
+    }
+
+    pub fn read_variable(&self, var: &String) -> Result<&i32, &'static str> {
+        match self.stack_variables.get(var) {
+            Some(op) => Ok(op),
+            None => Err("Variable does not exist"),
+        }
+    }
+
+    pub fn peek(&self) -> Result<&i32, &'static str> {
+        self.stack_array.last().ok_or("Empty stack")
+    }
+}
